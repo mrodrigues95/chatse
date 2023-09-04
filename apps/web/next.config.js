c0f4ca7a63b4ee49135b5ca17ad563baf0ddc309
mrodrigues95 <marcus.rodrigues95@gmail.com>
2023-09-04 15:02:18 -0400

    misc

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -7,16 +7,21 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
+	reactStrictMode: true,
+	experimental: {
+		appDir: true,
+		serverActions: true,
+	},
+	nx: {
+		// Set this to true if you would like to to use SVGR
+		// See: https://github.com/gregberge/svgr
+		svgr: false,
+	},
 };
 
 const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
+	// Add more Next.js plugins to this list if needed.
+	withNx,
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
