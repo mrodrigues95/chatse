1bc3f60f647dde757341500eb7168c07ae045f3e
Marcus Rodrigues <marcus.rodrigues95@gmail.com>
2023-09-30 15:45:44 +0000

    init storybook

---
+++ libs/toolkit/src/lib/toolkit.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ToolkitProps {}
+
+export function Toolkit(props: ToolkitProps) {
+  return (
+    <div className="text-green-500">
+      <h1>Welcome to Toolkit!</h1>
+    </div>
+  );
+}
+
+export default Toolkit;
---
+++ libs/toolkit/tailwind.config.js
@@ -0,0 +1,19 @@
+const { createGlobPatternsForDependencies } = require('@nx/next/tailwind.js');
+const path = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require('../../tailwind-workspace-preset.js')],
+  content: [
+    path.join(__dirname, 'src/**/*!(*.stories|*.spec).{js,jsx,ts,tsx,html,css}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
