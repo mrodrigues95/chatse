f6816824573aa009c82658915156cc0e0f238e39
Marcus Rodrigues <marcus.rodrigues95@gmail.com>
2023-12-17 14:29:44 +0000

    update deps

--- libs/toolkit/vite.config.ts
+++ libs/toolkit/vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  root: __dirname,
   cacheDir: '../../node_modules/.vite/toolkit',
 
   plugins: [
@@ -26,6 +27,9 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
+    outDir: '../../dist/libs/toolkit',
+    reportCompressedSize: true,
+    commonjsOptions: { transformMixedEsModules: true },
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
@@ -42,6 +46,11 @@ export default defineConfig({
   },
 
   test: {
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/libs/toolkit',
+      provider: 'v8',
+    },
     globals: true,
     cache: {
       dir: '../../node_modules/.vitest',
