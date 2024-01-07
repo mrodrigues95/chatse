aec313ac3644d6b956baebe3e0358f8abdfbac54
Marcus Rodrigues <marcus.rodrigues95@gmail.com>
2024-01-07 16:39:58 +0000

    finish accessible icon improvements

--- libs/toolkit/src/components/accessible-icon/accessible-icon.tsx
+++ libs/toolkit/src/components/accessible-icon/accessible-icon.tsx
@@ -1,5 +1,5 @@
 import { type ReactNode } from 'react';
-import { Slot } from '@radix-ui/react-slot';
+import { Slot, Slottable } from '@radix-ui/react-slot';
 
 export interface AccessibleIconProps {
   children: ReactNode;
@@ -7,28 +7,10 @@ export interface AccessibleIconProps {
   description?: string;
 }
 
-export const AccessibleIcon = ({ children, title, description }: AccessibleIconProps) => {
-  // const child = Children.only(children);
-
-  // const Component = Slot;
-
-  return (
-    <Slot aria-hidden={title ? undefined : true} role={title ? 'img' : undefined} focusable="false">
-      {/* {title && <title>{title}</title>}
-      {title && description && <desc>{description}</desc>} */}
-      {children}
-    </Slot>
-  );
-
-  // return cloneElement(
-  //   child as ReactElement,
-  //   {
-  //     'aria-hidden': title ? undefined : true,
-  //     role: title ? 'img' : undefined,
-  //     focusable: false,
-  //   },
-  //   // TODO: slot this.
-  //   // ...Children.toArray(child),
-  //   // title && <title>{title}</title>,
-  // );
-};
+export const AccessibleIcon = ({ children, title, description }: AccessibleIconProps) => (
+  <Slot aria-hidden={title ? undefined : true} role={title ? 'img' : undefined} tabIndex={-1}>
+    <Slottable>{children}</Slottable>
+    {title && <title>{title}</title>}
+    {title && description && <desc>{description}</desc>}
+  </Slot>
+);
